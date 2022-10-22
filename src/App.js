91b211efb4a95d00611475136ea73e501317fe0d
JongOne <cowhddnjsdl0@naver.com>
2022-10-22 18:20:29 +0900

    작업내역 update App.js File - 무한스크롤 구현 완료, 검색 아이콘 클릭시 인풋 포커스 구현 완료, axios 연동 완료(a 타입만)
    다음 작업 사항
    1. 라우터 연결하여 상세페이지 연결
    2. 상세페이지 작성
    3. 검색창 뷰 수정
    4. 검색창 필터 적용

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import { useInView } from "react-intersection-observer";
+import styled from "styled-components";
+import axios from "axios";
+import { CiSearch } from "react-icons/ci"
 
-function App() {
+const App = () => {
+  const [items, setItems] = useState([]);
+  const [page, setPage] = useState(0);
+  const [ref, inView] = useInView();
+
+  const InputRef = useRef(null);
+  const FocusInputRef = () => {
+    InputRef.current.focus();
+  }
+
+  const getItems = async () => {
+    await axios
+      .get(
+        `https://recruit-api.yonple.com/recruit/582962/a-posts?page=${page}`
+      )
+      .then((res) => {
+        setItems([...items, ...res.data]);
+      });
+  };
+
+  useEffect(() => {
+    getItems();
+  }, [page]);
+
+  useEffect(() => {
+    if (inView && items?.length !== 0) {
+      setPage(page + 1);
+    }
+  }, [inView]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Search>
+        <div><button onClick={() => { FocusInputRef(); }}><CiSearch style={{ fontSize: "30px", marginTop: "7px" }} /></button><input ref={InputRef} placeholder="검색어를 입력하세요." /></div>
+      </Search>
+      {items?.map((item) => (
+        <ItemBody key={item.id}>
+          <div>
+            <div style={{ fontWeight: "bold" }}><span style={{ color: "blue" }}>{item.id}</span>.{item.title}</div>
+          </div>
+          <ContetnBody>
+            {item.content}
+          </ContetnBody>
+        </ItemBody>
+      ))}
+      <div ref={ref}></div>
     </div>
   );
-}
+};
 
 export default App;
+
+const ItemBody = styled.div`
+  border: solid 5px gray;
+  width: 80%;
+  height: 90px;
+  margin: auto;
+`;
+
+const ContetnBody = styled.div`
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+`
+
+const Search = styled.div`
+  div {
+    border: solid 3px gray;
+  }
+  input {
+    margin-bottom: 50px;
+  }
+  width: 800px;
+  height: 50px;
+  display: flex;
+  margin: 20px auto 20px auto;
+  background-color: red;
+  :hover {
+    cursor: pointer;
+    
+  }
+`
